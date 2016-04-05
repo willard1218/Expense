73f2913d365385746a5522221a71df63f5e04f90
gangyu <imloed10000@gmail.com>
2016-04-05 18:51:25 +0800

    '...'

--- push.sh
+++ push.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-git add .
-git commit -m \"${1}\" 
-git push
-=======
 #!/bin/bash
-git add *
-git commit -m \"${1}\"
+git add .
+git commit -m \'${1}\'
+git status
 git push
->>>>>>> 98a9d0fd24c5842fd684f7f83dcacc0ea9d0ecb8
