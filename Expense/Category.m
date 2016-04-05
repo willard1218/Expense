d78562ce936ea6d380c68f332e09770d11f19930
willard1218 <willard1218@gmail.com>
2016-04-05 08:56:26 +0800

    new create helper for Category

--- Expense/Category.m
+++ Expense/Category.m
@@ -10,6 +10,16 @@
 
 @implementation Category
 
+
++ (void)createAndSaveEntity:(NSNumber *)categoryID name:(NSString *)name type:(NSNumber *)type order:(NSNumber *)order {
+  Category *category = [Category MR_createEntity];
+  category.categoryID = categoryID;
+  category.name = name;
+  category.type = type;
+  category.order = order;
+  [category.managedObjectContext MR_saveToPersistentStoreAndWait];
+}
+
 // Insert code here to add functionality to your managed object subclass
 
 @end
